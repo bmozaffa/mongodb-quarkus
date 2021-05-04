ff40e90785a49b795b7a10ac26fb4d72c8e23bc5
Jeremy Ary <jeremy.ary@gmail.com>
2021-05-03 23:46:31 -0500

    update param names to align with other connectors

--- src/main/java/org/acme/mongodb/servicebinding/MongoServiceBindingConverter.java
+++ src/main/java/org/acme/mongodb/servicebinding/MongoServiceBindingConverter.java
@@ -26,17 +26,17 @@ public class MongoServiceBindingConverter implements ServiceBindingConverter {
         ServiceBinding binding = matchingByType.get();
 
         Map<String, String> bindings = binding.getProperties();
-        String user = bindings.get("db.user");
-        String password = bindings.get("db.password");
-        String host = bindings.getOrDefault("db.host", "localhost");
-        String port = bindings.get("db.port");
+        String user = bindings.get("username");
+        String password = bindings.get("password");
+        String host = bindings.getOrDefault("host", "localhost");
+        String port = bindings.get("port");
         String hostPort;
         if( port == null ) {
             hostPort = host;
         } else {
             hostPort = String.format("%s:%s", host, port);
         }
-        String database = bindings.getOrDefault("db.name", "");
+        String database = bindings.getOrDefault("database", "");
         String mongoConnectionString;
         if( user == null ) {
             mongoConnectionString = String.format("mongodb+srv://%s/%s?retryWrites=true&w=majority", hostPort, database);
